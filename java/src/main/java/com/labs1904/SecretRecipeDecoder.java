460c9bb194253d28c180c80aac6350fbf94e7a68
vaishnavi <vaishu.srini15@gmail.com>
2022-03-04 09:55:04 -0600

    Tried to resolve

--- java/src/main/java/com/labs1904/SecretRecipeDecoder.java
+++ java/src/main/java/com/labs1904/SecretRecipeDecoder.java
@@ -53,9 +53,43 @@ public class SecretRecipeDecoder {
      */
     public static String decodeString(String str) {
         // TODO: implement me
-        return "";
-    }
+//        String[] dStr  = {};
 
+
+//            for (String i: ENCODING.keySet()) {
+//
+//                System.out.println(ENCODING.values());
+//            }
+
+//        Object value = ENCODING.get(str);
+
+
+//        for(int i = 0;i< str.length();i++){
+//            if(ENCODING.containsKey(str)){
+//                System.out.println(ENCODING.values());
+//                System.out.println(ENCODING.containsValue(str.charAt(i)));
+//            }
+//
+//        }
+
+//        for (int i = 0, n = str.length(); i < n; i++) {
+//            char c = str.charAt(i);
+//            System.out.println(ENCODING.containsValue(str.charAt(i)));
+//        }
+
+
+        for (int i = 0, n = str.length(); i < n; i++) {
+            char c = str.charAt(i);
+            System.out.println(c);
+            System.out.println(ENCODING.replace(str,ENCODING.get(c)));
+            System.out.println(ENCODING.values());
+            if(ENCODING.containsKey(c)){
+                System.out.println(ENCODING.values());
+            }
+
+        }
+        return str;
+    }
     /**
      * Given an ingredient, decode the amount and description, and return a new Ingredient
      * @param line
@@ -68,5 +102,34 @@ public class SecretRecipeDecoder {
 
     public static void main(String[] args) {
         // TODO: implement me
+
+        System.out.println(ENCODING.entrySet());
+        String str ="hgiif";
+        System.out.println(str);
+
+//        for (String i : ENCODING.keySet()) {
+//            if (String[i].equals(ENCODING.keySet())) {
+//
+//            }
+//            System.out.println(i);
+//        }
+
+
+
+
+//        str.split(str);
+//
+//        System.out.println(ENCODING.get(str));
+
+        System.out.println(decodeString("hgiif"));
+
+
+
+
+//        str.split(str);
+//
+//        System.out.println(ENCODING.get(str));
+
+//        System.out.println(decodeString("hgiif"));
     }
 }
